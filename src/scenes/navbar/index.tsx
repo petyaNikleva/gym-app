2e919776ac61c4ed1244a4a406795b920c58a3ab
P N <82761006+petyaNikleva@users.noreply.github.com>
2023-08-15 17:36:26 +0300

    add ActionButon shared component

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -4,6 +4,7 @@ import Logo from "@/assets/Logo.png";
 import Link from "./Link";
 import { SelectedPage } from "@/shared/type";
 import useMediaQuery from "@/hooks/useMediaQuery";
+import ActionButton from "@/shared/ActionButton";
 
 
 type Props = {
@@ -16,60 +17,64 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
   const [isMenuToggled, setIsMenuTogged] = useState<boolean>(false);
   const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
 
-  return <nav>
-    <div
-      className={`${flexBetween} fixed top-0 z-30 w-full py-6`}
-    >
-      <div className={`${flexBetween} mx-auto w-5/6`}>
-        <div className={`${flexBetween} w-full gap-16`}>
+  return (
+    <nav>
+      <div
+        className={`${flexBetween} fixed top-0 z-30 w-full py-6`}
+      >
+        <div className={`${flexBetween} mx-auto w-5/6`}>
+          <div className={`${flexBetween} w-full gap-16`}>
 
-          {/* Left Side */}
-          <img alt="logo" src={Logo} />
+            {/* Left Side */}
+            <img alt="logo" src={Logo} />
 
-          {/* Right Side */}
-          {isAboveMediumScreens ? (
-            <div className={`${flexBetween} w-full `}>
-              <div className={`${flexBetween} gap-8 text-sm `}>
-                <Link
-                  page="Home"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
-                <Link
-                  page="Benefits"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
-                <Link
-                  page="Our Classes"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
-                <Link
-                  page="Contact us"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
+            {/* Right Side */}
+            {isAboveMediumScreens ? (
+              <div className={`${flexBetween} w-full `}>
+                <div className={`${flexBetween} gap-8 text-sm `}>
+                  <Link
+                    page="Home"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Benefits"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Our Classes"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Contact us"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                </div>
+                <div className={`${flexBetween} gap-8`}>
+                  <p>Sign in</p>
+                  <ActionButton setSelectedPage={setSelectedPage}>
+                    Become a Member
+                  </ActionButton>
+                </div>
               </div>
-              <div className={`${flexBetween} gap-8`}>
-                <p>Sign in</p>
-                <button>Become a member</button>
+            ) : (
+              <div>
+                <button
+                  className="rounded-full bg-secondary-500 p-2"
+                  onClick={() => setIsMenuTogged(!isMenuToggled)}
+                >
+                  <Bars3Icon className="h-6 w-6 text-white" />
+                </button>
               </div>
-            </div>
-          ) : (
-            <div>
-              <button
-                className="rounded-full bg-secondary-500 p-2"
-                onClick={() => setIsMenuTogged(!isMenuToggled)}
-              >
-                <Bars3Icon className="h-6 w-6 text-white" />
-              </button>
-            </div>
-          )}
+            )}
+          </div>
         </div>
       </div>
-    </div>
-  </nav>;
+    </nav>
+  );
 };
 
 export default Navbar;
